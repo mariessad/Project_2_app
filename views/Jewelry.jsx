7ef28c7d69f5f5503370097e3f2fe876807d55a1
mariessad <74148632+mariessad@users.noreply.github.com>
2022-10-02 17:52:09 -0400

    add delete to jewelry page, add classnames

--- views/Jewelry.jsx
+++ views/Jewelry.jsx
@@ -6,16 +6,23 @@ class Jewelry extends React.Component {
     const { jewelry } = this.props;
     return (
       <DefaultLayout title={"Jewelry"}>
+        <a href={"/jewelry/new"}>Add New Jewelry listing</a>
         {jewelry.map((jewelry, i) => {
           return (
-            <div key={i}>
-              <p>{jewelry.title}</p>
-              <p>${jewelry.price}</p>
+            <div className="product-container" key={i}>
+              <p className="product-title">{jewelry.title}</p>
               <img className="product-img" src={`${jewelry.image}`}></img>
+              <p className="product-price">${jewelry.price}</p>
               <button>Add to Cart</button>
               <a href={`jewelry/${jewelry._id}/edit`}>
                 <button>Edit</button>{" "}
               </a>
+              <form
+                action={`/jewelry/${jewelry._id}?_method=DELETE`}
+                method="POST"
+              >
+                <input type="submit" value="DELETE" />
+              </form>
             </div>
           );
         })}
